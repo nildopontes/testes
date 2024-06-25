d9091e03804b19bfc948da44194ce95370e2af62
Nildo Vieira Pontes <nildo.pontes@bol.com.br>
2024-06-25 17:38:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-google.accounts.id.initialize({
-  client_id: '613616399292-i7njqr6ek4t8grt7ft3c3e6airkldn3e.apps.googleusercontent.com',
-  callback: handleCredentialResponse
-});
-function handleCredentialResponse(response) {
-  if (response.credential) {
-    console.log('Credenciais do usuário:', response.credential);
-  } else {
-    console.log('O usuário não fez login.');
-  }
+/**
+  Função executada quando o login é efetuado com sucesso
+*/
+function handleCredentialResponse(googleUser) {
+    console.log(googleUser);
 }
-document.getElementById('login-button').addEventListener('click', function() {
-  google.accounts.id.prompt(notification => {
-    if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
-      console.log('O usuário não visualizou ou pulou a notificação.');
-    } else {
-      console.log('O usuário interagiu com a notificação.');
-    }
-  });
-});
+
+/**
+  Função executada quando ocorrer falha no logn
+*/
+function onFailure(error) {
+    console.log(error);
+}
+
+/**
+  Função de deslogar o usuário
+*/
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
