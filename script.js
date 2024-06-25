50afcae3f1420bc4eecb0f78059693a577556a0e
Nildo Vieira Pontes <nildo.pontes@bol.com.br>
2024-06-25 15:25:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
 /**
-  Renderiza o botão de login na tela
-*/
-function renderButton() {
-    gapi.signin2.render('meu-botao', {
-        'scope': 'https://www.googleapis.com/auth/userinfo.email, https://www.googleapis.com/auth/userinfo.profile, openid', // solicitando acesso ao profile e ao e-mail do usuário
-        'width': 250,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
-    });
-}
-
-/**
   Função executada quando o login é efetuado com sucesso
 */
-function onSuccess(googleUser) {
-    // Recuperando o profile do usuário
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log("Name: " + profile.getName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-    // Recuperando o token do usuario. Essa informação você necessita passar para seu backend
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
+function handleCredentialResponse(googleUser) {
+   console.log(googleUser);
 }
 
 /**
